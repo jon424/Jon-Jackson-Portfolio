d6eecc543ed0dd8ccca7dd3daa5386877980d402
Jon <jonathanjackson424@gmail.com>
2020-06-30 14:58:41 -0400

    updated portfolio

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const slideIndex = [1, 1, 1];
-const slideId = ['mySlides1', 'mySlides2', 'mySlides3'];
+const slideIndex = [1, 1, 1, 1];
+const slideId = ['mySlides1', 'mySlides2', 'mySlides3', 'mySlides4'];
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
+showSlides(1, 3);
+
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
